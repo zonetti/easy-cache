75bfb7e7b1a3d8d88e99ec6e9f1956c8e5016487
Osvaldo Zonetti <zonetg3@gmail.com>
2013-11-20 09:36:07 -0200

    test fix

--- test/tests.js
+++ test/tests.js
@@ -25,9 +25,11 @@ describe('Tests', function() {
 
   it('should set a key that expires', function(done) {
     cache.set('timer', 'value', 100);
-    cache.get('timer').should.equal('value');
-    cache.exists('timer').should.be.true;
-    cache.getSize().should.equal(1);
+    setTimeout(function() {
+      cache.get('timer').should.equal('value');
+      cache.exists('timer').should.be.true;
+      cache.getSize().should.equal(1);
+    }, 50);
     setTimeout(function() {
       cache.exists('timer').should.be.false;
       cache.getSize().should.equal(0);
@@ -37,7 +39,7 @@ describe('Tests', function() {
         err.message.should.be.equal('Invalid key: timer');
         done();
       }
-    }, 300);
+    }, 150);
   });
 
   it('should unset a key', function() {
